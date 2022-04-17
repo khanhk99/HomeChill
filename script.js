0e3b320b99a69243e98941ba7ffb4cadad853894
Duc Khanh <khanhk99.tn@gmail.com>
2022-04-17 22:13:53 +0700

    change api value

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 // Get data via API
 function authenticate() {
     return gapi.auth2.getAuthInstance()
-        .signIn({
-            scope: "https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/spreadsheets.readonly"
-        })
+        .signIn({ scope: "https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/spreadsheets.readonly" })
         .then(function () { console.log("Sign-in successful"); },
             function (err) { console.error("Error signing in", err); });
 }
@@ -15,8 +13,9 @@ function loadClient() {
 }
 // Make sure the client is loaded and sign-in is complete before calling this method.
 function execute() {
-    return gapi.client.sheets.spreadsheets.get({
-        "spreadsheetId": "1DDsu1UGX5bN9PHKLYZBRCmjrsttInkGeWDCG8iW3MCM"
+    return gapi.client.sheets.spreadsheets.values.get({
+        "spreadsheetId": "1DDsu1UGX5bN9PHKLYZBRCmjrsttInkGeWDCG8iW3MCM",
+        "range": "0"
     })
         .then(function (response) {
             // Handle the results here (response.result has the parsed body).
